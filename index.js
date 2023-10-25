b717cc0cdb06688615c8e839e9e1def74b69d198
Quazi Shams Kabir <quaziraj12@gmail.com>
2023-10-25 23:41:00 +0530

    added db

--- index.js
+++ index.js
@@ -1,53 +1,164 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import _ from "lodash";
+import 'dotenv/config'
 
 const app = express();
 const port = 3000;
-
-var homeItems = [];
-var workItems = [];
+const fullDate = new Date().toLocaleString("en-US", {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+});
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// for the first time there will be no newItem as items is not initiated at all, but it'll not crash as
-// in ejs, locals.newItem will check for it, and no fallback is added if it not found, no it simply stays same
+// connecting mongoose with mongodb
+mongoose.connect(`mongodb+srv://shamsKabir:${process.env.ATLAS_PASS}@todo-app.bjbepcd.mongodb.net/?retryWrites=true&w=majority`);
 
-// after getting redirected here again from the post method, it now contain newItem, and send it to ejs file and gets displayed
+// simple schema for our items
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+// custom schema if the user creates a new list
+const customListSchema = new mongoose.Schema({
+  name: String,
+  newItems: [itemsSchema],
+});
+
+// creating Item model, for default route
+const Item = mongoose.model("Item", itemsSchema);
+
+// custom List model for custom routes
+const List = mongoose.model("List", customListSchema);
+
+// adding some default items
+const item1 = new Item({
+  name: "Welcome to your todo app",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", (req, res) => {
-  const fullDate = new Date().toLocaleString("en-US", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-  });
-  // sending page variable to later identify on which page user is on and route accordingly
-  res.render("index.ejs", {
-    date: fullDate,
-    newItem: homeItems,
-    page: "today",
-  });
-});
-
-app.get("/work", (req, res) => {
-  res.render("index.ejs", {
-    workItem: workItems,
-    page: "work",
+  Item.find({}).then((foundItems) => {
+    // inserting defaultItems, if there are no items in the db
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems).then(() => {
+        res.redirect("/");
+      });
+    } else {
+      // render the page with those items after redirecting of if the db already contain items
+      res.render("index.ejs", {
+        pageTitle: fullDate,
+        newItem: foundItems,
+      });
+    }
   });
 });
 
-// redirected instead of render solves :
-// 1. not adding the last element again, as refreshing it redirected to the homepage itself, not post route, so items dont get added here,
-// 2. respective pages(get route) reflect changes, not only post route
+// /favicon.ico, /robots.txt, /humans.txt, /sitemap.xml, /ads.txt: these are automatically routed by browser
+// and will create new mongodb document, to prevent it, top level domain should be avoided(/:param)
+// sub-level like /new/:param or need to manually handle all those but in future and other top level domain will also create the same problem
 
+app.get("/new/:customList", (req, res) => {
+  // using lodash captalize the first letter, it'll remove case-sensitivity of the listName
+  // and retriving will be easy
+  const customListName = _.capitalize(req.params.customList);
+
+  List.findOne({ name: customListName }).then((foundList) => {
+    if (!foundList) {
+      const list = new List({
+        name: customListName,
+        newItems: defaultItems,
+      });
+      list.save().then(() => {
+        res.redirect("/new/" + customListName);
+      });
+    } else {
+      res.render("index.ejs", {
+        pageTitle: foundList.name,
+        newItem: foundList.newItems,
+      });
+    }
+  });
+});
+
+app.get("/yourLists", (req, res) => {
+  List.find({}).then((foundList) => {
+    var listNames = [];
+    foundList.forEach((list) => {
+      listNames.push(list.name);
+    });
+    res.render("index.ejs", {
+      pageTitle: "Your Lists",
+      customItem: listNames,
+    });
+  });
+});
+
+app.post("/delete", (req, res) => {
+  const id = req.body.checkbox;
+  const title = req.body.title;
+
+  // as the today or fulldate list is the home page, docs from here can be directly deleted
+  // whereas customLists needed to be identified with its name, that'll go to else condition and search for that list
+  if (title === fullDate) {
+    Item.findByIdAndRemove(id).then(() => {
+      res.redirect("/");
+    });
+  } else {
+    // using mongodb $pull to remove item from the array of items (for loop or other methods can be used)
+    List.findOneAndUpdate(
+      { name: title },
+      { $pull: { newItems: { _id: id } } }
+    ).then(() => {
+      List.findByIdAndRemove(id).then(() => {
+        res.redirect("/new/" + title);
+      });
+    });
+  }
+});
+
+app.post("/deleteList", (req, res) => {
+  List.deleteOne({name:req.body.submit}).then(()=>{
+    res.redirect("/yourLists");
+  });
+});
+
+// inserting user entered item into db
 app.post("/", (req, res) => {
-  homeItems.push(req.body["newItem"]);
-  res.redirect("/");
-});
+  const itemName = req.body["newItem"];
+  const title = req.body.button;
 
-app.post("/work", (req, res) => {
-  workItems.push(req.body["newItem"]);
-  res.redirect("/work");
+  const item = new Item({
+    name: itemName,
+  });
+
+  if (title === fullDate) {
+    item.save().then(() => {
+      res.redirect("/");
+    });
+  } else if (title === "Your Lists") {
+    res.redirect("/new/" + itemName);
+  } else {
+    List.findOne({ name: title }).then((foundList) => {
+      foundList.newItems.push(item);
+      foundList.save().then(() => {
+        res.redirect("/new/" + title);
+      });
+    });
+  }
 });
 
 app.listen(port, () => {
